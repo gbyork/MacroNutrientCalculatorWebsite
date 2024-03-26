65d54dee24151bcf923f643dc2f605fb2dd990d3
Garrett York <gbyork18@gmail.com>
2024-03-26 16:53:52 -0500

    Login

---
+++ MacroNutrientCalculatorV2/Areas/Identity/Data/MacronutrientDBContext.cs
@@ -0,0 +1,35 @@
+﻿using MacroNutrientCalculatorV2.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MacroNutrientCalculatorV2.Areas.Identity.Data;
+
+public class MacronutrientDBContext : IdentityDbContext<MacroNutrientUser>
+{
+    public MacronutrientDBContext(DbContextOptions<MacronutrientDBContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+
+    }
+}
+
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<MacroNutrientUser>
+{
+    public void Configure(EntityTypeBuilder<MacroNutrientUser> builder)
+    {
+        builder.Property(x => x.FirstName).IsRequired().HasMaxLength(100);
+        builder.Property(x => x.LastName).IsRequired().HasMaxLength(100);
+
+    }
+}
